afaf37d35ac200a6a567ced36ebb2187f4cd9afa
Adam Alcott <aalcott14@gmail.com>
2017-07-06 14:46:27 -0500

    switch to bootstrap tabs for nav and align content

--- js/animation.js
+++ js/animation.js
@@ -14,6 +14,15 @@ $(document).ready(function() {
     $(".typeIntro").hide();
     $(".home").fadeIn("slow");
   });
+
+  $(".tab-menu>.list-group>a").click(function(e) {
+    e.preventDefault();
+    $(this).siblings('a.active').removeClass("active");
+    $(this).addClass("active");
+    var index = $(this).index();
+    $(".tab>.tab-content").removeClass("active");
+    $(".tab>.tab-content").eq(index).addClass("active");
+  });
 });
 
 function type(string) {

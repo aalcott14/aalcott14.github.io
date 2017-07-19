8e0d403ce1361124b9d1667a0a60fed41398ef1c
Adam Alcott <aalcott14@gmail.com>
2017-07-19 15:50:14 -0500

    add codeup slide to portfolio

--- js/carousel.js
+++ js/carousel.js
@@ -24,4 +24,30 @@ $(document).ready(function(){
     $(".right").click(function(){
         $("#myCarousel").carousel("next");
     });
+
+    $("#myCarousel2").carousel('pause');
+
+    $(".item1").click(function(){
+        $("#myCarousel2").carousel(0);
+    });
+
+    $(".item2").click(function(){
+        $("#myCarousel2").carousel(1);
+    });
+
+    $(".item3").click(function(){
+        $("#myCarousel2").carousel(2);
+    });
+
+    $(".item4").click(function(){
+        $("#myCarousel2").carousel(3);
+    });
+
+    $(".left").click(function(){
+        $("#myCarousel2").carousel("prev");
+    });
+
+    $(".right").click(function(){
+        $("#myCarousel2").carousel("next");
+    });
 });
